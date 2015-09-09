9f57131f21e68ab5efd37bb00605feb0cf08385c
bakarisanja <milos.babic@mi-system.co.rs>
2015-09-10 00:17:21 +0200

    ispravljen deo logina

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -13,10 +13,11 @@ class Login extends CI_Controller
 	 */
 	public function index()
 	{
+		
 		$this->load->model('Model_admin');
 
-		if($token = $this->Model_admin->checkAdmin('slavka1','5449abce9417344bb63eb38fa6d7419f')){
-			$set_token = array('token' => $this->token);
+		if($token = $this->Model_admin->checkAdmin('slavka','5449abce9417344bb63eb38fa6d7419f')){
+			$set_token = array('token' => $token);
 			$this->session->set_userdata($set_token);
 			redirect('admin');
 		} else {
